91f75aead82a18441d147d46d7d0cd1897b7d1c9
Rick Wilde <rkwilde@gmail.com>
2018-02-20 15:28:06 -0700

    first draft without guidance

---
+++ index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>360 Canopy</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Slab" rel="stylesheet"> 
+    <style>
+        /*div{
+            outline: 1px solid green;
+        }*/
+        .debug-border{ border: 1px solid green; }
+        .background-pic1{
+            background-image: url("working_files/weddingtent3.jpg");
+            background-position: center top;
+            background-repeat: no-repeat;
+            background-color: black;
+            background-attachment: fixed;
+        } /*.height-pic1{ height: 740px }*/
+        .background-pic2{
+            background-image: url("working_files/weddingtent3.jpg");
+            background-position: center center;
+            background-repeat: no-repeat;
+            background-color: black;
+        }
+        .border-bottom{
+            border-bottom: 1px solid black;
+        }
+        .color-bg1{
+            background-color: black;
+        }
+        .color-bg2{
+            background-color: white;
+        }
+        .color-font1{
+            color: white;
+        }
+        .font-med{
+            font-size: 1.2em;
+        }
+        .font-med2{
+            font-size: 1.4em;
+        }
+        .font-med3{
+            font-size: 1.6em;
+        }
+        .font-big{
+            font-size: 4em;
+        }
+        .font-body1{
+            font-family: 'Josefin Slab', serif;
+            /*text-transform: uppercase;*/
+            font-size: 1.5em;
+            color: white;
+        }
+        .font-topmenu{
+            font-family: 'Josefin Slab';
+            text-transform: uppercase;
+            font-size: 1.1em;
+            color: white;
+        }
+        .font-emph1{
+            font-family: 'Times New Roman';
+            font-style: italic;
+            font-size: 3.5em;
+            color: white;
+        }
+        .font-pretty1{
+            font-family: 'Josefin Slab', serif;
+        }
+        .font-pretty2{
+            font-family: 'Times New Roman', serif;
+        }
+        .font-uppercase{
+            text-transform: uppercase;
+        }
+        .padding-big1{
+            padding-top: 2em;
+            padding-bottom: 3em;
+        }
+        .link-pretty{
+            text-transform: uppercase;
+            color: #a300cc;
+            text-decoration: underline;
+        } .link-pretty:hover{
+            color: white;
+        }
+        .padding-big2{
+            padding: 3em;
+        }
+        .text-center{
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container-fluid background-pic1 border-bottom">
+        <div class="container">
+            <div class="row justify-content-around py-2">
+                <div class="col-1 align-self-end pb-2 text-center font-topmenu">about</div>
+                <div class="col-1 align-self-end pb-2 text-center font-topmenu">products</div>
+                <div class="col-2">
+                    <img src="working_files/logo_white.png" class="img-fluid">
+                </div>
+                <div class="col-1 align-self-end pb-2 text-center font-topmenu">maintenance</div>
+                <div class="col-1 align-self-end pb-2 text-center font-topmenu">contact</div>
+            </div>
+        </div>
+        <div class="container">
+            <p class="font-emph1 padding-big1">Your outdoor wedding, gala, or party should be as memorable as a work of art...</p>
+        </div>
+    </div>
+    <div class="container-fluid color-bg1 border-bottom">
+        <div class="container">
+            <p class="font-body1 text-center py-4"> 
+                At Classical Tents &amp; Party Goods, the Berkshires’ premier source for tent rentals, we are here to help make your event a success. Whether you’re planning a rustic dream wedding, an anniversary party, a nonprofit fund-raiser, a trade show, or a corporate reception, we’ll help you bring your vision to vibrant, dimensional life.
+            </p>
+        </div>
+    </div>
+    <div class="container-fluid background-pic2">
+        <div class="row">
+            <div class="col-1 color-bg2">
+            </div>
+            <div class="col-3 color-bg2 py-5 pr-5">
+                <h1 class="font-pretty1 font-big">Customized Service</h1>
+                <p class="font-pretty2 font-med">
+                    Through a customized planning process that takes into account the size, style, and theme of your event, we’ll help you select your tent style and accessories, like tent flooring and staging. Plan a visit to our Pittsfield showroom to choose from our tents, party furniture, and party supplies, including tables, chairs, bars, linens, vintage china, glassware, and decorative accessories. The Classical Tents team will help you edit your ideas into a seamless, one-of-a-kind event, plus deliver, install, and remove your items.
+                </p>
+                <p class="font-pretty2 font-med">
+                    Classical Tents serves the Berkshires and other counties in Massachusetts, as well as neighboring areas of Connecticut, Vermont, and New York.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid background-pic2">
+        <div class="row justify-content-end">
+            <div class="col-3 color-bg2 py-5 pl-5">
+                <h1 class="font-pretty1 font-big">
+                    Products &amp; Services
+                </h1>
+                <p class="font-pretty2 font-med">
+                    We know that planning an event, especially if it’s in an unfamiliar location, can be complicated and time-consuming. We go beyond simple party rentals, and are here to support you in planning your special day.
+                </p>
+                <p class="font-pretty2 font-med">
+                    <a href="#" class="link-pretty">SEE PRICING GUIDE</a>
+                </p>
+            </div>
+            <div class="col-1 color-bg2"></div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row justify-content-around py-5">
+            <div class="col-3">
+                <h1 class="font-pretty1 font-med2">A Wedding at Stonover Farm</h1>
+                <p class="font-pretty2">
+                    When Nicolina and Luke decided they wanted to exchange their wedding vows in the Berkshires, and found that they wanted their ceremony to be held in the stunning St. Ann’s Church…
+                </p>
+                <p class="font-pretty2">
+                    <a href="#" class="link-pretty">READ MORE</a>
+                </p>
+            </div>
+            <div class="col-3">
+                <h1 class="font-pretty1 font-med2">Wheatleigh Wedding</h1>
+                <p class="font-pretty2">
+                    Carol and Joel chose to host their intimate wedding at Wheatleigh, with an indoor ceremony and cocktail hour, and a formal diningner in the dining room afterwards. For their indoor ceremony…
+                </p>
+                <p class="font-pretty2">
+                    <a href="#" class="link-pretty">READ MORE</a>
+                </p>
+            </div>
+            <div class="col-3">
+                <h1 class="font-pretty1 font-med2">A Summer Wedding at the Mount</h1>
+                <p class="font-pretty2">
+                    The Mount in Lenox, MA is always an elegant choice for a couple’s Berkshires wedding, and this wedding proves no different.  Kristen and Zach chose The Mount for their small affair, and following…
+                </p>
+                <p class="font-pretty2">
+                    <a href="#" class="link-pretty">READ MORE</a>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid color-bg1 border-bottom">
+        <div class="container py-4">
+            <div class="row">
+                <div class="col-8">
+                    <p class="font-pretty2 color-font1 font-med3"> 
+                        Ready to design the wedding or special event of your dreams?
+                    </p>
+                    <p class="font-pretty2 color-font1 font-med3">
+                        Get started with Classical Tents!
+                    </p>
+                </div>
+                <div class="col-4 pt-4">
+                    <button type="button" class="btn btn-secondary">CONTACT US</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</body>
+
+</html>
